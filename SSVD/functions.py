4b5e155c453a451ae487d264263c35c0d013585e
huangxe19 <filament1224@hotmail.com>
2021-04-27 19:29:21 -0400

    add more files

--- SSVD/functions.py
+++ SSVD/functions.py
@@ -2,7 +2,7 @@ import scipy.linalg as la
 import numpy as np
 import scipy.linalg as la
 
-def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
+def SSVD_single(x, gamma1 = 2, gamma2 = 2, tol = 1e-6, max_iter = 50):
     
     n, d = x.shape
     
@@ -10,7 +10,8 @@ def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
     U, S, V = la.svd(x)
     Vt = V.T
     iters = 0
-    converge_diff = tol + 1
+    converge_diff_u = tol + 1
+    converge_diff_v = tol + 1
     
     U_old = U[:, 0]
     S_old = S[0]
@@ -20,19 +21,20 @@ def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
     U_new = np.zeros(n)
     
     # Step 2
-    while(converge_diff > tol and iters < max_iter):
+    while(converge_diff_u > tol and converge_diff_v > tol and iters < max_iter):
                 
+        iters += 1
         #update v
         V_hat = np.zeros(d)
         Xt_U = x.T @ U_old
-        omega_2 = np.abs(Xt_U) ** (-gamma)
+        omega_2 = np.abs(Xt_U) ** (-gamma2)
         error_var = np.abs(np.sum(x ** 2) - sum(Xt_U**2))/(n*d-d)
-        lambda2s = np.unique(np.append(np.abs(Xt_U / omega_2), 0))
-        lambda2s.sort()
+        lambda_2 = np.unique(np.append(np.abs(Xt_U / omega_2), 0))
+        lambda_2.sort()
         
-        min_bic = 10000
+        min_bic = np.Inf
         
-        for l in lambda2s:
+        for l in lambda_2:
             
             ## Find all v's
             term1 = Xt_U / abs(Xt_U)
@@ -41,7 +43,8 @@ def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
             V_hat = term1 * term2           
    
             ## Choose the best v based on bic
-            bic = np.sum((x - U_old.reshape((-1, 1)) @ V_hat.reshape((1, -1)))**2) / error_var + np.sum(V_hat!=0) * np.log(n * d)
+            bic = np.sum((x - U_old.reshape((-1, 1)) @
+                          V_hat.reshape((1, -1)))**2) / error_var + np.sum(V_hat!=0) * np.log(n * d)
             if bic < min_bic:
                 V_new = V_hat
                 min_bic = bic
@@ -53,14 +56,14 @@ def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
         U_hat = np.zeros(n)
         X_V = x @ V_old
         
-        omega_1 = np.abs(X_V) ** (-gamma)
+        omega_1 = np.abs(X_V) ** (-gamma1)
         error_var = np.abs(np.sum(x ** 2) - sum(X_V**2))/(n*d-n)
-        lambda1s = np.unique(np.append(np.abs(X_V / omega_1), 0))
-        lambda1s.sort()
+        lambda_1 = np.unique(np.append(np.abs(X_V / omega_1), 0))
+        lambda_1.sort()
         
-        min_bic = 10000
+        min_bic = np.Inf
         
-        for l in lambda1s:
+        for l in lambda_1:
 
             term1 = X_V / abs(X_V)
             term2 = abs(X_V) - l * omega_1 / 2
@@ -68,7 +71,8 @@ def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
             U_hat = term1 * term2
             
             ## Choose the best v based on bic
-            bic = np.sum((x - U_hat.reshape((-1, 1)) @ V_old.reshape((1, -1)))**2)/error_var + np.sum(U_hat!=0) * np.log(n * d)
+            bic = np.sum((x - U_hat.reshape((-1, 1)) @
+                          V_old.reshape((1, -1)))**2)/error_var + np.sum(U_hat!=0) * np.log(n * d)
             if bic < min_bic:
                 U_new = U_hat
                 min_bic = bic
@@ -76,13 +80,12 @@ def SSVD_single(x, gamma = 2, tol = 1e-6, max_iter = 50):
         s = np.linalg.norm(U_new)
         U_new = U_new / s
                 
-        converge_diff = np.sqrt(np.sum((U_new - U_old) ** 2) + np.sum((V_new - V_old) ** 2))
-        iters += 1
+        converge_diff_u = np.sqrt(np.sum((U_new - U_old) ** 2))
+        converge_diff_v = np.sqrt(np.sum((V_new - V_old) ** 2))
         U_old = U_new
         V_old = V_new
                 
-    return U_new, V_new, iters
-
+    return U_new, S_old, V_new, iters
 
 
 def SSVD_multi_layer(x, layers):
@@ -90,15 +93,15 @@ def SSVD_multi_layer(x, layers):
     n, d = x.shape
     all_layers_u = np.zeros((n, layers))
     all_layers_v = np.zeros((d, layers))
+    all_layers_s = np.zeros(layers)
     
     for i in range(layers):
-        u_new, v_new, iters = SSVD_new(x)
-        s = u_new @ x @ v_new.T
-        layer = s * u_new.reshape((-1, 1)) @ v_new.reshape((1, -1))
+        u_new, s_new, v_new, iters = SSVD_single(x)
+        layer = s_new * u_new.reshape((-1, 1)) @ v_new.reshape((1, -1))
         all_layers_u[:, i] = u_new
         all_layers_v[:, i] = v_new
+        all_layers_s[i] = s_new
         x = x - layer
-        print("layer ", i, " iteration", iters)
         
     return all_layers_u, all_layers_v
     
