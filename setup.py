ae5e39ecb86f491b0d465c164f9da228b0c6012c
huangxe19 <filament1224@hotmail.com>
2021-04-27 22:36:57 +0000

    modify setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-import setuptools
-import versioneer
+from setuptools import setup, find_packages
+from distutils.util import convert_path
 
-
+main_ns = {}
+ver_path = convert_path('SSVD/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 setuptools.setup(
     name="STA663-SSVD",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version=main_ns['__version__'],
     url='https://github.com/RumoZhang/STA663-SSVD',
     author="Rumo Zhang, Xige Huang",
     author_email="xh90@duke.edu",
